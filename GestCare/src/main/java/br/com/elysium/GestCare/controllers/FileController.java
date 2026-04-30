a7719c9c446fea4bd4d13ae04a83d52e7dde1ba3
Gumoro <gustavo.gumoroestudos@gmail.com>
2026-04-30 16:33:19 -0300

    feat: upload de documentos

--- GestCare/src/main/java/br/com/elysium/GestCare/controllers/FileController.java
+++ GestCare/src/main/java/br/com/elysium/GestCare/controllers/FileController.java
@@ -2,10 +2,10 @@ package br.com.elysium.GestCare.controllers;
 import br.com.elysium.GestCare.model.File;
 import br.com.elysium.GestCare.services.FileServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.util.List;
 
 
@@ -19,6 +19,17 @@ public class FileController {
     @GetMapping("/patient/{id}")
     public List<File> getFilesByPatient(@PathVariable Long id) {
         return fileService.getFilesByPatient(id);
+    }
+
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public File uploadFile(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("hospitalId") Long hospitalId,
+            @RequestParam("fileTypeId") Long fileTypeId,
+            @RequestParam("fileDate") String fileDate,
+            @RequestParam("description") String description
+    ) {
+        return fileService.saveFile(file, hospitalId, fileTypeId, fileDate, description);
     }
 
 }
--- GestCare/src/main/java/br/com/elysium/GestCare/services/FileServices.java
+++ GestCare/src/main/java/br/com/elysium/GestCare/services/FileServices.java
@@ -1,12 +1,24 @@
 package br.com.elysium.GestCare.services;
 import br.com.elysium.GestCare.model.File;
+import br.com.elysium.GestCare.model.FileType;
+import br.com.elysium.GestCare.model.Hospital;
+import br.com.elysium.GestCare.model.Patient;
 import br.com.elysium.GestCare.repositories.FileRepository;
+import br.com.elysium.GestCare.repositories.FileTypeRepository;
+import br.com.elysium.GestCare.repositories.HospitalRepository;
+import br.com.elysium.GestCare.repositories.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class FileServices {
+
+    @Autowired
+    private PatientRepository patientRepository;
 
     @Autowired
     private FileRepository fileRepository;
@@ -15,4 +27,62 @@ public class FileServices {
         return fileRepository.findFilesByPatientId(patientId);
     }
 
+    @Autowired
+    private HospitalRepository hospitalRepository;
+
+    @Autowired
+    private FileTypeRepository fileTypeRepository;
+
+    public File saveFile(MultipartFile multipartFile,
+                         Long hospitalId,
+                         Long fileTypeId,
+                         String fileDate,
+                         String description) {
+
+        try {
+            String uploadDir = System.getProperty("user.dir") + "/uploads/";
+
+            java.io.File dir = new java.io.File(uploadDir);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+
+            String originalName = multipartFile.getOriginalFilename();
+
+
+            String fileName = originalName
+                    .replaceAll("\\s+", "_")
+                    .replaceAll("[^a-zA-Z0-9._-]", "");
+
+
+            String filePath = uploadDir + fileName;
+
+
+            multipartFile.transferTo(new java.io.File(filePath));
+
+
+            Hospital hospital = hospitalRepository.findById(hospitalId).orElseThrow();
+            FileType fileType = fileTypeRepository.findById(fileTypeId).orElseThrow();
+
+            Patient patient = patientRepository.findById(1L).orElseThrow();
+
+
+            File file = new File();
+            file.setTitle(fileName);
+            file.setFileName(fileName);
+            file.setFilePath(filePath);
+            file.setDescription(description);
+            file.setFileDate(LocalDate.parse(fileDate));
+            file.setHospital(hospital);
+            file.setFileType(fileType);
+
+            file.setDoctorName("Dr. Teste");
+            file.setDoctorCrm("123456");
+
+            return fileRepository.save(file);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao salvar arquivo: " + e.getMessage());
+        }
+    }
 }
